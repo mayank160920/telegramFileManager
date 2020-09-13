8023c0918310fc9e37d596f77efdb85edbe46718
Nicu Borta <nicuborta123@tutanota.com>
2020-09-13 11:51:27 +0300

    src/cli.py: some cleanup

--- src/cli.py
+++ src/cli.py
@@ -6,7 +6,7 @@ import asyncio
 import misc
 from backend.sessionsHandler import SessionsHandler
 
-class UserInterface(SessionsHandler):
+class UserInterface():
     def __init__(self):
         self.cfg = misc.loadConfig()
 
@@ -15,25 +15,16 @@ class UserInterface(SessionsHandler):
         except ValueError:
             realChannelID = self.cfg['telegram']['channel_id']
 
-        super().__init__(
-            realChannelID, self.cfg['telegram']['api_id'],
-            self.cfg['telegram']['api_hash'], self.cfg['paths']['data_path'],
-            self.cfg['paths']['tmp_path'], int(self.cfg['telegram']['max_sessions'])
-        )
+        #super().__init__(
+        #    realChannelID, self.cfg['telegram']['api_id'],
+        #    self.cfg['telegram']['api_hash'], self.cfg['paths']['data_path'],
+        #    self.cfg['paths']['tmp_path'], int(self.cfg['telegram']['max_sessions'])
+        #)
 
         self.loop = asyncio.get_event_loop()
 
-        main_widget = build_widgets()
 
-        urwid_loop = urwid.MainLoop(
-            main_widget,
-            event_loop=urwid.AsyncioEventLoop(loop=self.loop),
-            unhandled_input=urwid_unhandled,
-        )
-        urwid_loop.run()
-
-
-    def build_widgets(self):
+    def build_main_widgets(self):
         def update_progress(widget_ref):
             widget = widget_ref()
             if not widget:
@@ -45,22 +36,33 @@ class UserInterface(SessionsHandler):
             # Schedule to update the clock again in one second
             loop.call_later(1, update_progress, widget_ref)
 
-        progress = urwid.Text('')
-        update_progress(weakref.ref(progress))
+        title = urwid.Text("Telegram File Manager", align='center')
+        #update_progress(weakref.ref(progress))
 
-        return urwid.Filler(progress, 'top')
+        return urwid.Filler(title, 'top')
 
 
-    def unhandled(key):
-        optionDict = {'upload' : {'value' : False, 'keybind' : ord(self.cfg['keybinds']['upload']), 'function' : self.uploadHandler},
-                      'download' : {'value' : False, 'keybind' : ord(self.cfg['keybinds']['download']), 'function' : self.downloadHandler},
-                      'cancel' : {'value' : False, 'keybind' : ord(self.cfg['keybinds']['cancel']), 'function' : self.cancelHandler},
-                      'resume' : {'value' : False, 'keybind' : ord(self.cfg['keybinds']['resume']), 'function' : self.resumeHandlerUI}}
+    def unhandled_keys(self, key):
+        #optionDict = {'upload' : {'value' : False, 'keybind' : ord(self.cfg['keybinds']['upload']), 'function' : self.uploadHandler},
+        #              'download' : {'value' : False, 'keybind' : ord(self.cfg['keybinds']['download']), 'function' : self.downloadHandler},
+        #              'cancel' : {'value' : False, 'keybind' : ord(self.cfg['keybinds']['cancel']), 'function' : self.cancelHandler},
+        #              'resume' : {'value' : False, 'keybind' : ord(self.cfg['keybinds']['resume']), 'function' : self.resumeHandlerUI}}
 
         if key == 'esc':
-            self.endSessions() # Must call to exit the program
+            #self.endSessions() # Must call to exit the program
             raise urwid.ExitMainLoop
+
+    def main(self):
+        main_widget = self.build_main_widgets()
+
+        self.urwid_loop = urwid.MainLoop(
+            main_widget,
+            event_loop=urwid.AsyncioEventLoop(loop=self.loop),
+            unhandled_input=self.unhandled_keys
+        )
+        self.urwid_loop.run()
 
 
 if __name__ == "__main__":
     ui = UserInterface()
+    ui.main()
